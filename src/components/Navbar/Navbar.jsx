94359aed3786678a6afcf8d4e89a91cb6e45bf64
tomitose <tomitosello@gmail.com>
2023-03-06 15:05:35 -0300

    Agregado de logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,23 @@
-import React from 'react';
-import Link from '@mui/material/Link';
-import { Avatar } from '@material-ui/core';
+import React from "react";
+import Link from "@mui/material/Link";
+import { Avatar } from "@material-ui/core";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className='container-nav'>
-        <nav className='nav'>
-            <div className='nav-left'>
-                <ul>
-                    <Avatar alt="Material UI logo" src="./vite.svg" />                    <Link>Home</Link>
-                    <Link>Works</Link>
-                    <Link>Contact</Link>
-                </ul>
-            </div>
-            <div className='nav-right'>
-
-            </div>
-        </nav>    
+    <div className="container-nav">
+      <nav className="nav">
+        <div className="nav-left">
+          <ul>
+            <Avatar alt="Material UI logo" src="./logoFrame.svg" style={{width:"70px",height:"70px"}}/> <Link>Home</Link>
+            <Link>Works</Link>
+            <Link>Contact</Link>
+          </ul>
+        </div>
+        <div className="nav-right"></div>
+      </nav>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
